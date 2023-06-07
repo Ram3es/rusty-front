9986b153242b93bfb6bafa4a2e0bdb314844251b
dyl-kh <dylanjkhan@hotmail.com>
2023-06-07 11:19:33 +1000

    add tooltip arrow

--- src/components/battle/CaseToolTip.jsx
+++ src/components/battle/CaseToolTip.jsx
@@ -5,8 +5,8 @@ import { getCurrencyString } from '../mines_new/utils/tools'
 
 const CaseToolTip = (props) => {
   return (
-    <div 
-    class='font-SpaceGrotesk font-semibold min-w-[167px] flex gap-1 items-center justify-center py-[6px] rounded flex-col text-[12px]
+    <div
+    class='relative font-SpaceGrotesk font-semibold min-w-[167px] flex gap-1 items-center justify-center py-[6px] rounded flex-col text-[12px]
     text-shadow-lg shadow-sm shadow-black ' 
     style={{
       color: 'rgba(162, 165, 198, 1)', 
@@ -26,8 +26,25 @@ const CaseToolTip = (props) => {
    Right Click for drops
    </span>
  </div>
-  </div>
+ {/* <div class="absolute w-0 h-0 -bottom-2  border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-[#242945]"/> */}
+ <div 
+  class="absolute -bottom-[11px] text-10 text-[#242945] " 
+  style={{
+    display: 'inline-block',
+    "-webkit-transform": 'scale(2,1)', /* Safari and Chrome */
+    "-moz-transform": 'scale(2,1)', /* Firefox */
+    "-ms-transform": 'scale(2,1)', /* IE 9 */
+    "-o-transform": 'scale(2,1)', /* Opera */
+    transform: 'scale(2,1)', /* W3C */
+    'text-shadow': '20 20 50px black'
+  }}
+>
+  ▼
+</div>
+</div>
   )
+
+  // 232843
 }
 
-export default CaseToolTip
+export default CaseToolTip;
