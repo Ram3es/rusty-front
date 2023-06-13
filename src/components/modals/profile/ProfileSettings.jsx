d3728095a10f1d965d41feded77318838cd549a0
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-06-13 18:21:12 +0300

    add lang switcher

--- src/components/modals/profile/ProfileSettings.jsx
+++ src/components/modals/profile/ProfileSettings.jsx
@@ -9,6 +9,7 @@ import EsFlag from '../../../assets/img/header/esFlag.svg'
 import RuFlag from '../../../assets/img/header/ruFlag.svg'
 import { playButtonClickSound, playMenuToggle } from '../../../utilities/Sounds/SoundButtonClick'
 import GrayGradientButton from '../../elements/GrayGradientButton'
+import ArrowDown from '../../icons/ArrowDown'
 
 const ProfileSettings = (props) => {
   const i18n = useI18n()
@@ -102,9 +103,9 @@ const ProfileSettings = (props) => {
 
   onMount(() => {
     setAvailableLocales([
-      { title: 'En', code: 'en', active: isSelected('en'), flag: EnFlag },
-      { title: 'Es', code: 'es', active: isSelected('es'), flag: EsFlag },
-      { title: 'Ru', code: 'ru', active: isSelected('ru'), flag: RuFlag }
+      { title: 'English', code: 'en', active: isSelected('en'), flag: EnFlag },
+      { title: 'Spain', code: 'es', active: isSelected('es'), flag: EsFlag },
+      { title: 'Russian', code: 'ru', active: isSelected('ru'), flag: RuFlag }
     ])
   })
 
@@ -182,65 +183,40 @@ const ProfileSettings = (props) => {
           </GrayGradientButton>
         </div>
       </div>
-      <div class='w-full flex justify-between items-center'>
-        <div class='flex flex-col'>
-          <p class='text-18 text-white font-bold font-Oswald uppercase'>
-            {i18n.t('profile_true.settings.Language')}
-          </p>
-          <p class='text-14 text-gray-8c font-normal cursor-pointer'>
-            {i18n.t('profile_true.settings.Used on the website')}
-          </p>
-        </div>
+      <div class='flex flex-col w-full gap-2'>
+        <p class='text-14 text-gray-9a font-medium font-SpaceGrotesk capitalize'>
+          {i18n.t('profile_true.settings.Language')}
+        </p>
         <div class='relative'>
-          <button
-            type='button'
-            onClick={() => {
+          <GrayGradientButton
+            additionalClass='w-[154px] h-10 justify-between text-gray-9a pl-4 px-3 pr-3 gap-[35px]'
+            callbackFn={() => {
               playMenuToggle()
               setLangModalOpen((prev) => !prev)
             }}
-            ref={langButtonMain}
-            class='relative w-60 h-10 flex justify-between items-center py-2 pl-3 text-left bg-dark-20 rounded-2'
             aria-haspopup='listbox'
             aria-expanded='true'
             aria-labelledby='listbox-label'
           >
-            <span class='block truncate'>
-              <For each={availableLocales()}>
-                {(item) =>
-                  item.active ? (
-                    <span class='flex gap-2 items-center font-Oswald text-14 text-white uppercase'>
-                      {' '}
-                      <img src={item.flag} alt='flag' />
-                      {item.title}
-                    </span>
-                  ) : (
-                    ''
-                  )
-                }
-              </For>
-            </span>
-            <span class={`pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2`}>
-              <svg
-                class={`duration-200 ${isLangModalOpen() ? '' : 'transform -rotate-180'}`}
-                width='13'
-                height='7'
-                viewBox='0 0 13 7'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  d='M6.12452 -7.90072e-07C5.90864 -7.61763e-07 5.69271 0.0803387 5.52806 0.240654L0.346806 5.29049C0.0172113 5.61172 0.0172114 6.13254 0.346807 6.45364C0.676269 6.77473 1.21054 6.77473 1.54017 6.45364L6.12452 1.98533L10.7091 6.45332C11.0385 6.77442 11.5729 6.77442 11.9023 6.45332C12.2321 6.13222 12.2321 5.61157 11.9023 5.29034L6.72115 0.240471C6.55642 0.0801826 6.34048 -8.18392e-07 6.12452 -7.90072e-07Z'
-                  fill='#48488B'
-                />
-              </svg>
-            </span>
-          </button>
-
+            <For each={availableLocales()}>
+              {(item) =>
+                item.active && (
+                  <span class='flex gap-[8.5px] items-center font-SpaceGrotesk text-14 text-gray-9a capitalize'>
+                    <img class='w-[16.5px] h-[11.55px]' src={item.flag} alt='flag' />
+                    {item.title}
+                  </span>
+                )
+              }
+            </For>
+            <GrayGradientButton additionalClass='w-4 h-4 shadow-button text-gray-9a' noShadow>
+              <ArrowDown isOpen={isLangModalOpen()} />
+            </GrayGradientButton>
+          </GrayGradientButton>
           <ul
             ref={langWrapperMain}
             class={`${
               isLangModalOpen() ? '' : 'hidden'
-            } absolute z-40 mt-1 w-full overflow-auto py-1 font-Oswald text-14 text-white uppercase bg-dark-20 rounded-2`}
+            } absolute w-[154px] flex flex-col gap-2 z-40 mt-1 bg-gray-button-gradient border-[#FFFFFF0A] border transition duration-300 overflow-auto p-2 font-SpaceGrotesk text-14 text-gray-9a capitalize bg-dark-20 rounded-4`}
             tabindex='-1'
             role='listbox'
             aria-labelledby='listbox-label'
@@ -248,25 +224,27 @@ const ProfileSettings = (props) => {
           >
             <For each={availableLocales()}>
               {(item) =>
-                !item.active ? (
+                !item.active && (
                   <li
                     onClick={() => toggleActiveLang(item.code)}
-                    class='text-gray-900 relative select-none py-2 pl-3 pr-9 cursor-pointer'
                     id='listbox-option-0'
                     role='option'
                   >
-                    <span class='flex gap-1 items-center font-Oswald text-14 text-white uppercase'>
-                      <img src={item.flag} alt='flag' />
+                    <span class='flex gap-[8.5px] items-center rounded-4 border-[#FFFFFF0A] border p-1'>
+                      <img class='w-[16.5px] h-[11.55px]' src={item.flag} alt='flag' />
                       {item.title}
                     </span>
                   </li>
-                ) : (
-                  ''
                 )
               }
             </For>
           </ul>
         </div>
+        {/* <Dropdown
+          activeName={availableLocales().find(item => item.active).title}
+          itemsList={availableLocales()}
+          submitItem={() => toggleActiveLang(1)}
+        /> */}
       </div>
 
       <div class='flex flex-col w-full'>
