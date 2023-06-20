4ec8cc92124b7e1c5c3c6f7abf7b3c1a9fe37fa7
Alexey Tsukanov <alexey.t@itsua.org>
2023-06-20 14:54:55 +0300

    add home page information

--- src/components/new-home/FreeRewards.jsx
+++ src/components/new-home/FreeRewards.jsx
@@ -1,4 +1,47 @@
 import { For } from "solid-js";
+import Bajoonga from "../../assets/img/home/rewards/Bajoonga.png"
+import FreeCases from "../../assets/img/home/rewards/FreeCases.png"
+import FlashCodes from "../../assets/img/home/rewards/FlashCodes.png"
+import CreatorCode from "../../assets/img/home/rewards/CreatorCode.png"
+import DiscordGiveaways from "../../assets/img/home/rewards/DiscordGiveaways.png"
+import TriviaEvents from "../../assets/img/home/rewards/TriviaEvents.png"
+import ClaimRakeback from "../../assets/img/home/rewards/ClaimRakeback.png"
+import TwitterGiveaways from "../../assets/img/home/rewards/TwitterGiveaways.png"
+
+const rewards = [
+    {
+        name: "Climb the Bajoonga",
+        image: Bajoonga
+    },
+    {
+        name: "Unbox Daily Free Cases",
+        image: FreeCases
+    },
+    {
+        name: "Redeem \nFlash Codes",
+        image: FlashCodes
+    },
+    {
+        name: "Redeem Creator Code",
+        image: CreatorCode
+    },
+    {
+        name: "Discord Giveaways",
+        image: DiscordGiveaways
+    },
+    {
+        name: "Trivia \nEvents",
+        image: TriviaEvents
+    },
+    {
+        name: "Instantly Claim Rakeback",
+        image: ClaimRakeback
+    },
+    {
+        name: "Twitter Giveaways",
+        image: TwitterGiveaways
+    }
+]
 
 const FreeRewards  = () => {
     return (
@@ -8,8 +51,23 @@ const FreeRewards  = () => {
                 <span class=''>Explore all the rewards we offer to our players!</span>
             </div>
             <div class='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 w-full gap-4'>
-                <For each={Array.from({ length: 8 }, (_,idx) => idx)} >
-                    {(index) => <div class=' h-[68px] home-rewards'></div> }
+                <For each={rewards} >
+                    {(r) => <div class='home-rewards flex items-center flex-col py-1 px-4 overflow-hidden justify-between'>
+                        {r.image ? <><img
+                        src={r.image}
+                        alt="rotating dome"
+                        class="h-16"
+                        />
+                        <img
+                        src={r.image}
+                        alt="rotating dome"
+                        class="absolute h-28 right-1/2 top-1/4 -rotate-12 opacity-5"
+                        />
+                        </> : ""}
+                        <div class="text-center text-13 font-SpaceGrotesk font-bold text-gray-9a whitespace-pre-line">
+                            {r.name}
+                        </div>
+                    </div> }
                 </For>
             </div>
         </div>
--- src/components/new-home/RustyOriginals.jsx
+++ src/components/new-home/RustyOriginals.jsx
@@ -1,10 +1,78 @@
 import { For } from "solid-js";
 import BattleIcon from '../icons/BattleIcon'
+import CaseBattles from "../../assets/video/CaseBattles.webm"
+import Coinflip from "../../assets/video/Coinflip.webm"
+import WheelPlh from "../../assets/img/home/wheelPlaceholder.png" 
+import Upgrader from "../../assets/video/Upgrader_1.webm"
+import Mines from "../../assets/video/Mines_1.webm"
+import CaseOpening from "../../assets/video/CaseOpening_1.webm"
+import Plinko from "../../assets/video/Plinko_1_1.webm"
+import PVPMines from "../../assets/video/VS_1.webm"
+import PVPMinesIcon from '../icons/PVPMinesIcon'
+import CoinflipIcon from '../icons/CoinflipIcon'
+import UpgraderIcon from '../icons/UpgraderIcon'
+import WheelIcon from '../icons/WheelIcon'
+import CaseOpeningIcon from '../icons/CaseOpeningIcon'
+import MinesIcon from '../icons/MinesIcon'
+import PlinkoIcon from '../icons/PlinkoIcon'
+import { URL } from "../../libraries/url";
+import { NavLink } from "solid-app-router";
+const gameModes = [
+  {
+    name: "Case Battles",
+    video: CaseBattles,
+    icon: <BattleIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.CASE_BATTLES,
+  }, 
+  {
+    name: "Coinflip",
+    video: Coinflip,
+    icon: <CoinflipIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.COINFLIP,
+  },
+  {
+    name: "Wheel",
+    image: WheelPlh,
+    disabled: true,
+    icon: <WheelIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.WHEEL
+  },
+  {
+    name: "Upgrader",
+    video: Upgrader,
+    icon: <UpgraderIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.UPGRADER,
+  },
+  {
+    name: "Mines",
+    video: Mines,
+    icon: <MinesIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.MINES
+  },
+  {
+    name: "Cases",
+    video: CaseOpening,
+    icon: <CaseOpeningIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.UNBOXING
+  },
+  {
+    name: "Plinko",
+    video: Plinko,
+    icon: <PlinkoIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.PLINKO
+  },
+  {
+    name: "PVP Mines",
+    video: PVPMines,
+    icon: <PVPMinesIcon additionClasses='w-5 h-5 text-gray-55' />,
+    url: URL.GAMEMODES.PVP_MINES
+  }
+]
 
 export const TilesLabel = (props) => {
 
     return (
-      <div class='absolute top-0 left-0 w-[142px] h-10 center shadow-button home-tile-label '>
+      <div class='absolute top-0 left-0 px-4 h-10 center shadow-button home-tile-label '>
         {props.children}
       </div>
     )
@@ -19,28 +87,62 @@ const RustyOriginals = () => {
             <span class=''>Try out our fun & immersive gamemodes!</span>
         </div>
         <div class="grid grid-cols-home-original gap-x-6 gap-y-8 w-full ">
-            <For each={Array.from({length: 3}, (_,i) => i + 1)}>
-                {(index) => 
-                <div class="col-span-5 home-originals h-[200px]">
-                  <TilesLabel >
-                    <div class='flex items-center gap-2 text-gray-9a font-SpaceGrotesk font-bold'>
-                      <BattleIcon  additionClasses='w-5 h-5 text-gray-55' />
-                      <span>Case Battles</span>
-                    </div>
-                  </TilesLabel>
+            <For each={[...gameModes].slice(0, 3)}>
+                {(mode) => 
+                <div class={`col-span-5 home-originals overflow-hidden ${mode.disabled ? "opacity-60 grayscale-[98%] sepia-[8%] hue-rotate-[167deg] saturate-[628%]" : ""}`}>
+                  <NavLink href={mode.url}>
+                    {mode.video ? <video
+                      src={mode.video}
+                      alt="rotating dome"
+                      class="w-full"
+                      onMouseEnter={e => e.target.play()}
+                      onMouseLeave={e => {
+                        e.target.pause()
+                        e.target.currentTime = 0
+                      }}
+                      playsinline
+                      muted
+                    /> : ""}
+                    {mode.image ? <img
+                      src={mode.image}
+                      alt="rotating dome"
+                      class="w-full"
+                    /> : ""}
+                    <TilesLabel >
+                      <div class='flex items-center gap-2 text-gray-9a font-SpaceGrotesk font-bold'>
+                        {mode.icon}
+                        <span>{mode.name}</span>
+                      </div>
+                    </TilesLabel>
+                  </NavLink>
                 </div>
               }
             </For>
-            <For each={Array.from({length: 5}, (_,i) => i + 1)}>
-              {(index) => 
-                <div class=" col-span-5 lg:col-span-3 home-originals-secondary h-[272px] relative ">
-                  <TilesLabel >
-                    <div class='flex items-center gap-2 text-gray-9a font-SpaceGrotesk font-bold'>
-                      <BattleIcon  additionClasses='w-5 h-5 text-gray-55' />
-                      <span>Case Battles</span>
-                    </div>
-                  </TilesLabel>
-                </div>
+            <For each={[...gameModes].slice(3)}>
+              {(mode) => 
+                
+                  <div class=" col-span-5 lg:col-span-3 home-originals-secondary h-[272px] overflow-hidden relative ">
+                    <NavLink href={mode.url}>
+                      <video
+                        src={mode.video}
+                        alt="rotating dome"
+                        class="min-w-full min-h-full"
+                        onMouseEnter={e => e.target.play()}
+                        onMouseLeave={e => {
+                          e.target.pause()
+                          e.target.currentTime = 0
+                        }}
+                        playsinline
+                        muted
+                      />
+                      <TilesLabel >
+                        <div class='flex items-center gap-2 text-gray-9a font-SpaceGrotesk font-bold'>
+                          {mode.icon}
+                          <span>{mode.name}</span>
+                        </div>
+                      </TilesLabel>
+                    </NavLink>
+                  </div>
               }
             </For>
         </div>
