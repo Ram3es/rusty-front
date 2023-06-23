e1c53d11e8745253d4d19cd1cd2acb2ac7a491da
Alexey Tsukanov <alexey.t@itsua.org>
2023-06-23 11:43:23 +0300

    add timeaout, fix responsive

--- src/components/upgrader/dome/Dome.jsx
+++ src/components/upgrader/dome/Dome.jsx
@@ -9,9 +9,10 @@ import {
   activeItem,
   betValue,
   underOver,
+  isAnimationShown
 } from "../../../views/upgrader/Upgrader";
 
-const Dome = () => {
+const Dome = (props) => {
   return (
     <div class="relative flex items-center justify-center">
       <img src={DomeBg} alt="dome background" class="" />
@@ -46,7 +47,7 @@ const Dome = () => {
         class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         w-full max-w-[604px]"
       />
-         <div class='w-full absolute upgrader-wires animate top-[38%] scale-[35%] -translate-x-[27%] -translate-y-[42%] ' />
+         <div class={`w-full ${isAnimationShown() ? 'animate' : ''} absolute upgrader-wires -left-[80%] -top-[14%] scale-[20%] sm:-left-[68%] sm:-top-[4%] sm:scale-[25%] md:-left-[64%] md:-top-[2%] xxl:top-[38%] xxl:scale-[35%] xxl:-translate-x-[27%] xxl:-translate-y-[42%] xxl:left-auto `} />
       <Screen />
     </div>
   );
