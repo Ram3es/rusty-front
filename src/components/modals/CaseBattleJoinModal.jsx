edcded08ec4d5e923c1b6782f8fa3d95a0dc301f
Alexey Tsukanov <alexey.t@itsua.org>
2023-06-27 16:55:50 +0300

    add discound info

--- src/components/modals/CaseBattleJoinModal.jsx
+++ src/components/modals/CaseBattleJoinModal.jsx
@@ -243,6 +243,14 @@ const CaseBattleJoinModal = (props) => {
                 ) : (
                   <BattleGroupIcon additionClasses='text-[#5AC3FF] w-5' />
                 )}
+                {props.game?.fundBattle ? <div
+                  class={"rounded-2 absolute right-0.5 -top-1 -left-5 z-10 px-1 -rotate-[10deg] w-14 center text-green-3e font-Quicksand font-bold text-14"}
+                  style={{
+                      background: "linear-gradient(75.96deg, rgba(255, 255, 255, 0) 20.07%, rgba(255, 255, 255, 0.12) 41.3%, rgba(0, 0, 0, 0.12) 68.93%, rgba(255, 255, 255, 0.12) 100%), radial-gradient(98.73% 114.02% at 100% -37.29%, rgba(11, 189, 82, 0.48) 0%, rgba(0, 0, 0, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, radial-gradient(99.15% 99.15% at 12.7% 107.2%, rgba(11, 189, 82, 0.48) 0%, rgba(0, 0, 0, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, linear-gradient(0deg, rgba(255, 255, 255, 0.08), rgba(255, 255, 255, 0.08)), linear-gradient(180deg, rgba(11, 189, 82, 0) 0%, rgba(11, 189, 82, 0.12) 100%), radial-gradient(58.03% 60.37% at 50% 29.27%, rgba(118, 124, 255, 0.05) 0%, rgba(118, 124, 255, 0) 100%), radial-gradient(100% 275.07% at 100% 0%, rgba(33, 36, 60, 0.48) 0%, rgba(29, 31, 48, 0.48) 100%)",            
+                  }}
+                  >
+                    -{props.game?.fundPercent}%
+                </div> : ""}
                 <span
                   class={`text-yellow-ffb font-SpaceGrotesk text-16 font-bold`}
                   classList={{
@@ -255,7 +263,7 @@ const CaseBattleJoinModal = (props) => {
                 </span>
                 <Coin width='5' />
                 <span class='text-gradient'>{props.game?.fundBattle ? 
-                  props.game?.totalValue - (props.game?.totalValue * (props.game?.fundPercent / 100)) : 
+                  (props.game?.totalValue - (props.game?.totalValue * (props.game?.fundPercent / 100))).toFixed() : 
                   setup().borrowMoney ? 
                   props.game?.totalValue - getProportionalPartByAmount(
                     props.game?.totalValue,
