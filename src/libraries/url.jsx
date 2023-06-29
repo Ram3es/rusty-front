b378129fe99c089d8f8cf139d400c5e99579c7bc
Andrii Rastorhuiev <andrii.rastorhuiev@itsua.eu>
2023-06-29 18:40:31 +0300

    fix url

--- src/libraries/url.jsx
+++ src/libraries/url.jsx
@@ -1,7 +1,7 @@
 export const TESTMODE = true;
-const PATH = TESTMODE ? "https://b2d7-93-76-248-70.ngrok-free.app/" : "https://rustyloot.gg";
+// const PATH = TESTMODE ? "https://b2d7-93-76-248-70.ngrok-free.app/" : "https://rustyloot.gg";
 // const PATH = TESTMODE ? "http://localhost:1337" : "https://rustyloot.gg";
-// const PATH = TESTMODE ? "http://157.245.38.175" : "https://rustyloot.gg";
+const PATH = TESTMODE ? "http://157.245.38.175" : "https://rustyloot.gg";
 
 export const API = `${PATH}`;
 
